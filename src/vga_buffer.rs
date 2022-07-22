d267da6371c46000597418bbd4a26055c288ac99
Au = Or [79] <assanisaidelamani@mail.com>
2022-07-22 18:28:59 -0400

    Ajout de macro d'impression println! print! _print dans vga_buffer.rs

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -161,3 +161,24 @@ impl fmt::Write for Writer {
         Ok(())
     }
 }
+
+//ecriture des macro
+//L' #[macro_export]attribut rend la macro disponible pour l'ensemble du crate (pas seulement le module auquel il est défini) et les crates externes
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+//La _printfonction verrouille notre statique WRITERet appelle la write_fmtméthode dessus. Cette méthode provient du Writetrait, nous devons importer ce trait. 
+//doc(hidden)attribut pour le masquer de la documentation générée.
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
